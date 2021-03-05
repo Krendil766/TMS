468e714395954db75b5af07ddb2ff247ae7b9da3
Krendil766 <krendil766@gmail.com>
2021-03-06 02:41:06 +0300

    Added new tasks and refactor rename components

---
+++ src/components/05.03/index.js
@@ -0,0 +1,15 @@
+import Task16 from './Task16/';
+import Task17 from './Task17/';
+
+import './0503.css'
+
+export default function Day0503() {
+    return (
+        <div className = 'day-02-03'>
+            <h1>05/03/21</h1>
+            <Task16 />
+            <div style = {{marginBottom:30+'px'}}></div>
+            <Task17 />
+        </div>
+    )
+}
--- src/components/25.02/Task8/index.js
+++ src/components/25.02/Task8/index.js
@@ -3,7 +3,7 @@ import './style.css'
 const Task8 = (props) => {
     const arr = props.arr;
     const listItems = arr.map(item =>
-        < li key={item._id.toString()} className='arr-name' ><h3>{item.name.first}</h3><h4>{item.name.last}</h4></ li>
+        < li style={{listStyleType:'none'}} key={item._id.toString()} className='arr-name' ><h3>{item.name.first}</h3><h4>{item.name.last}</h4></ li>
     )
     return (
         <div><ul>{listItems}</ul></div>
